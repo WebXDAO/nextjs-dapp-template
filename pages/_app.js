5e21f130728a76f0daf8e5019bd44c9a75decbd3
Maxime TFT <mkubik@tftlabs.com>
2022-01-06 11:24:00 +0100

    add: Navbar + Footer component - Update basic design.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import 'antd/dist/antd.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import {useEffect, useState } from 'react'
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
 
 export default function ConnectWallet() {
-
   const [address, setAddress] = useState([]);
 
   const connectToMetamask = async () => {
@@ -12,19 +13,28 @@ export default function ConnectWallet() {
         setAddress(accounts[0])
       }).catch((err) => console.log(err))
       : console.log("Please install MetaMask")
-    }
+  }
 
   return (
     <>
       <Head>
-        <title>TFTPDF Démo | Dashboard</title>
+        <title>Nextjs-Dapp-Template | Web3Community</title>
         <meta name="description" content="Bienvenue sur le dashboard" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>
-        <button onClick={connectToMetamask} variant="text">Connect Wallet</button>
-        <p>address = {address}</p>
+      <Navbar></Navbar>
+      <div className="mx-auto flex flex-col h-screen">
+        <div className="m-auto justify-center">
+          <button
+            type="button"
+            className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 inline-flex items-center px-6 py-3 text-base font-medium rounded-md shadow-sm text-white transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110"
+            onClick={connectToMetamask}
+          >
+            👛 Connect Wallet
+          </button>
+          <div className="font-medium">address = {address}</div>
+        </div>
       </div>
     </>
   )
